0be580e1defcab5b0f66b94d0ca37da1ad823bb2
Thang Tran <thang.tran@hotschedules.com>
2018-03-24 14:15:35 +0700

    - add: seed data

---
+++ seed.js
@@ -0,0 +1,60 @@
+const axios = require('axios');
+const getToken = require('./login');
+
+const env = process.env.NODE_ENV || 'dev';
+const namespace = process.env.NAMESPACE || 'kmsdev5';
+const BASE_URL = `https://api.bodhi-${env}.io/${namespace}/resources`;
+
+const documents = [
+  {
+    resourceName: 'InventoryItem',
+    data: require('./seed/item.json')
+  },
+  {
+    resourceName: 'ProductionSchedule',
+    data: require('./seed/schedule.json')
+  },
+  {
+    resourceName: 'ProductionItemThawSetup',
+    data: require('./seed/thawsetup.json')
+  },
+  {
+    resourceName: 'InventoryOnHand',
+    data: require('./seed/onhand.json')
+  }
+];
+
+const post = ({ resourceName, method = 'POST', token, data }) => axios({
+  url: `${BASE_URL}/${resourceName}`,
+  method,
+  headers: {
+    'Content-Type': 'application/json',
+    'Cookie': `ID_TOKEN=${token}`
+  },
+  data
+});
+
+const seedAll = token => documents.map(doc => {
+  return post({
+    resourceName: doc.resourceName,
+    token,
+    data: doc.data
+  })
+});
+
+getToken({
+  env,
+  username: `admin__${namespace}`,
+  password: `admin__${namespace}`
+}).then((token) => {
+  Promise.all(
+    seedAll(token)
+  ).then(resp => {
+    console.log('========================');
+    console.log('  SEED DONE !!!!!!!!!!!!');
+  }).catch(err => {
+    console.log('err: ', err);
+  });
+}).catch(loginError => {
+  console.log('loginError: ', loginError);
+});
