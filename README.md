88ec67a3e31b3537bbbf527217b60d6b0bbbce73
Thang Tran <thang.tran@hotschedules.com>
2018-03-28 08:01:43 +0700

    refactor :D

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # clear-data
-clear data by resourceName
+## This repo supporting re-seed data
 
-** npm run clear
+1. Update agent's info by modify or add `.env` in folder `env`.
+2. Update resources to be deleted in `src/clear.js`
+3. Update resources to be seeded in `src/seed.js`
+4. Run command: `npm run qa`
---
+++ src/clear.js
@@ -0,0 +1,38 @@
+const axios = require('axios');
+
+const resourceAsClear = [
+  'ProductionSuggestedThawQuantityJobLog',
+  'ProductionSuggestedThawItemPull',
+  'InventoryItem',
+  'ProductionSchedule',
+  'ProductionItemThawSetup',
+  'InventoryOnHand',
+];
+
+function clear() {
+  const baseUrl = this.baseUrl;
+  const token = this.token;
+  const storeKey = this.storeKey;
+
+  const singleClear = ({resourceName}) => {
+    const url = `${baseUrl}/${resourceName}?where={"store_key":"${storeKey}"}`;
+    return axios({
+      method: 'DELETE',
+      url,
+      headers: {
+        'Content-Type': 'application/json',
+        'Cookie': `ID_TOKEN=${token}`
+      }
+    })
+  };
+
+  const clearsAsPromise = resourceAsClear.map(resourceName => {
+    return singleClear({resourceName});
+  });
+
+  return Promise.all(
+    clearsAsPromise
+  )
+}
+
+module.exports = clear;
